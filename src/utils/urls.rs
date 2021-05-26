5f11a921979e85d4d6f6ef8a6134d9a338de7cbe
Oscuro87 <capelle.e@gmail.com>
2021-05-26 15:26:17 +0200

    Exception gif in link mover

--- src/utils/urls.rs
+++ src/utils/urls.rs
@@ -4,5 +4,10 @@ pub fn does_he_look_like_a_link(msg: String) -> bool {
     if let Err(_) = Url::parse(&msg) {
         return false;
     }
+
+    if msg.contains("tenor.com") || msg.contains("gif") || msg.contains("giphy.com") {
+        return false;
+    }
+
     true
 }
