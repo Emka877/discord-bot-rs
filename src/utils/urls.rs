d4ac7e31acec7a9e46036d7563c91b340cf75179
Oscuro87 <capelle.e@gmail.com>
2021-05-26 15:29:06 +0200

    added hardcoded exceptions in link mover

--- src/utils/urls.rs
+++ src/utils/urls.rs
@@ -1,12 +1,20 @@
 use url::Url;
 
 pub fn does_he_look_like_a_link(msg: String) -> bool {
+    let exceptions: Vec<&'static str> = vec![
+        "tenor.com",
+        "gif",
+        "giphy.com",
+    ];
+
     if let Err(_) = Url::parse(&msg) {
         return false;
     }
 
-    if msg.contains("tenor.com") || msg.contains("gif") || msg.contains("giphy.com") {
-        return false;
+    for exc in exceptions.iter() {
+        if msg.contains(exc) {
+            return false;
+        }
     }
 
     true
