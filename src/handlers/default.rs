5236ef32f75b8087502a9ec093ff2b6742e40016
Oscuro87 <capelle.e@gmail.com>
2021-05-26 14:07:20 +0200

    Update link mover

--- src/handlers/default.rs
+++ src/handlers/default.rs
@@ -1,4 +1,4 @@
-use std::{ops::Add, sync::Arc};
+use std::{ops::Add, result, sync::Arc};
 
 use chrono::{Timelike, Utc};
 use rand::{prelude::SliceRandom, thread_rng};
@@ -7,7 +7,7 @@ use serenity::{
     client::{Context, EventHandler},
     http::CacheHttp,
     model::{
-        channel::Message,
+        channel::{Channel, Message},
         id::{ChannelId, GuildId},
     },
 };
@@ -66,28 +66,26 @@ impl EventHandler for DefaultHandler {
         let dev_source_chanid: ChannelId = ChannelId(829346813357195304);
         let dev_target_chanid: ChannelId = ChannelId(847057541402066975);
         let content = msg.content.clone();
-        let mut original_author = msg.author_nick(&ctx.http).await;
 
         if msg.channel_id == source_chanid || msg.channel_id == dev_source_chanid {
-            if original_author.is_none() {
-                eprintln!("Cannot find original author of the link...");
-                original_author = Some("Unknown author".into());
-            }
-
             if does_he_look_like_a_link(content.clone()) {
                 if let Err(why) = msg.delete(&ctx.http).await {
                     eprintln!("{}", why);
                 }
 
-                let warn_msg: String = format!("No links here, I moved it to #liens for you!");
-                let out_msg: String = format!(
-                    "{} sent a link: {}",
-                    original_author.unwrap().clone(),
-                    content.clone()
-                );
+                let warn_msg: String = "J'ai bougé le lien que vous avez posté dans #liens!\nCe message s'autodétruira dans 10 secondes!".into();
+                let out_msg: String = format!("{} sent a link: {}", msg.author, content.clone());
 
-                if let Err(why) = msg.reply_mention(&ctx.http, warn_msg).await {
-                    eprintln!("{}", why);
+                let reply_result = msg.reply_mention(&ctx.http, warn_msg).await;
+                if reply_result.is_err() {
+                    eprintln!("{:?}", reply_result.err());
+                } else {
+                    let reply_to_delete = reply_result.unwrap();
+                    let ctx_copy = ctx.clone();
+                    tokio::spawn(async move {
+                        tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+                        let _ = reply_to_delete.delete(&ctx_copy.http).await;
+                    });
                 }
 
                 if source_chanid != dev_source_chanid {
