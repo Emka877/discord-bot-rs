28c1fadd36f3b4b324a2dae7e9928811f539ae78
Oscuro87 <capelle.e@gmail.com>
2021-06-18 17:31:18 +0200

    Update

--- src/buckets/utilities.rs
+++ src/buckets/utilities.rs
@@ -1,7 +1,7 @@
 use serenity::{client::Context, framework::standard::{CommandResult, macros::command}, model::channel::Message};
 use std::fs;
 use std::env::current_exe;
-use chrono::offset::Local;
+use chrono::offset::Utc;
 use chrono::DateTime;
 
 
@@ -9,13 +9,14 @@ use chrono::DateTime;
 pub async fn version(ctx: &Context, msg: &Message) -> CommandResult {
     let exe = current_exe().unwrap();
     let metas = fs::metadata(exe).unwrap();
-    let build_date: DateTime<Local> = metas.created().unwrap().into();
-    
+    let build_date: DateTime<Utc> = metas.created().unwrap().into();
+    let build_tz = build_date + chrono::Duration::hours(2);
+
     msg.reply(
         ctx, 
-        format!("Anna version {}.\nBuilt on {}", 
+        format!("\nAnna version {}.\nBuilt on {}", 
             env!("CARGO_PKG_VERSION"),
-            build_date
+            build_tz
         )
     ).await?;
     
