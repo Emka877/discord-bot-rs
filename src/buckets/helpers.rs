0e5a4bbce37def94f8b8d2f152a4bcc2a1907fdd
Oscuro87 <capelle.e@gmail.com>
2021-07-26 14:33:16 +0200

    Delete message after x secs to reduce spam.

--- src/buckets/helpers.rs
+++ src/buckets/helpers.rs
@@ -1,7 +1,9 @@
-use serenity::{client::Context, framework::{standard::{CommandResult, macros::command}}, model::channel::Message, utils::MessageBuilder};
+use std::sync::Arc;
 
-use crate::utils::SanitizedMessage;
+use serenity::{client::Context, framework::standard::{macros::command, CommandResult}, http::CacheHttp, model::channel::Message, utils::MessageBuilder};
+
 use crate::plugins::weather::{fetch_weather_for_city, kelvin_to_celsius};
+use crate::utils::SanitizedMessage;
 
 #[command]
 #[owners_only]
@@ -32,8 +34,9 @@ pub async fn links(ctx: &Context, msg: &Message) -> CommandResult {
 #[aliases(meteo, météo)]
 pub async fn weather(ctx: &Context, msg: &Message) -> CommandResult {
     let san_msg = SanitizedMessage::from(msg);
+    let delete_reply_after_secs: u64 = 60 * 2;
     let mut city: String = "".into();
-    
+
     if san_msg.are_any_arguments_present() {
         // City specified by user, pass it to the weather fetcher function
         city = san_msg.args_single_line;
@@ -42,28 +45,34 @@ pub async fn weather(ctx: &Context, msg: &Message) -> CommandResult {
     match fetch_weather_for_city(city).await {
         Ok(weather) => {
             if weather.weather.len() > 0 {
-                
                 let msg_builder = MessageBuilder::new()
                     .user(msg.author.id)
                     .push("\nMétéo à ")
                     .push_bold_line(weather.name)
                     .push_line(format!("Ciel: {}.", weather.weather[0].description))
-                    .push_line(format!("Il fait {:.1}°C ({:.1}°C ressenti).", 
-                        kelvin_to_celsius(weather.main.temp), 
+                    .push_line(format!(
+                        "Il fait {:.1}°C ({:.1}°C ressenti).",
+                        kelvin_to_celsius(weather.main.temp),
                         kelvin_to_celsius(weather.main.feels_like)
                     ))
                     .push_line(format!("Humidité {}%.", weather.main.humidity))
                     .build();
-                let _ = msg.channel_id.say(&ctx, msg_builder).await;
+                if let Ok(sent) = msg.channel_id.say(&ctx, msg_builder).await {
+                    let ctx_a = Arc::new(ctx.clone());
+                    tokio::spawn(async move {
+                        tokio::time::sleep(tokio::time::Duration::from_secs(delete_reply_after_secs)).await;
+                        let _ = sent.delete(&ctx_a.http()).await;
+                    });
+                }
             }
-        },
+        }
         Err(err) => {
             let msg_builder = MessageBuilder::new()
                 .user(msg.author.id)
                 .push_line(format!("Erreur: {}", err))
                 .build();
             let _ = msg.channel_id.say(&ctx, msg_builder).await;
-        },
+        }
     }
 
     Ok(())
--- src/plugins/fn_tea_time.rs
+++ src/plugins/fn_tea_time.rs
@@ -1,12 +1,12 @@
 #![allow(dead_code)]
 
-use std::sync::Arc;
-use rand::{prelude::SliceRandom, thread_rng};
 use chrono::{Timelike, Utc};
-use std::ops::Add;
+use rand::{prelude::SliceRandom, thread_rng};
 use serenity::{client::Context, model::id::ChannelId};
+use std::ops::Add;
+use std::sync::Arc;
 
-use crate::constants::channels::{ZIGGURAT};
+use crate::constants::channels::ZIGGURAT;
 
 pub async fn tea_time_announcer(ctx: Arc<Context>) -> () {
     tokio::spawn(async move {
@@ -18,7 +18,7 @@ pub async fn tea_time_announcer(ctx: Arc<Context>) -> () {
                 || (utc_plus_2.hour() == 22 && utc_plus_2.minute() < 1)
             {
                 if let Err(why) = ChannelId(ZIGGURAT)
-                    .send_message(&ctx, |m| { 
+                    .send_message(&ctx, |m| {
                         m.content("It's iced tea time!");
                         m.allowed_mentions(|am| am.parse(serenity::builder::ParseValue::Users));
                         m
