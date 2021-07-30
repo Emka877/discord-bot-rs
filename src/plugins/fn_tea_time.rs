3f13edea93c9d6bf73d5c6d991a0d8dc45d7d2e3
Oscuro87 <capelle.e@gmail.com>
2021-07-30 22:26:20 +0200

    tea time variance following temperature

--- src/plugins/fn_tea_time.rs
+++ src/plugins/fn_tea_time.rs
@@ -5,6 +5,7 @@ use rand::{prelude::SliceRandom, thread_rng};
 use serenity::{client::Context, model::id::ChannelId};
 use std::ops::Add;
 use std::sync::Arc;
+use super::weather::fetch_weather_default_city;
 
 use crate::constants::channels::ZIGGURAT;
 
@@ -13,13 +14,29 @@ pub async fn tea_time_announcer(ctx: Arc<Context>) -> () {
         loop {
             tokio::time::sleep(tokio::time::Duration::from_millis(60000)).await;
             let utc_plus_2 = Utc::now().add(chrono::Duration::hours(2));
+            
+            // Get the weather to decide for hot or cold beverage.
+            let cur_weather = fetch_weather_default_city().await;
+            let mut beverage: String = "tea".into();
+            if cur_weather.is_ok() {
+                let temperature: f32 = cur_weather.unwrap().main.temp;
+
+                beverage = match temperature {
+                    temp if temp <= 0.0 => "hot lava tea or chocolate".into(),
+                    temp if temp > 0.0 && temp < 21.0 => "tea".into(),
+                    temp if temp >= 21.0 && temp <= 30.0 => "iced tea".into(),
+                    temp if temp > 30.0 => "super frozen tea".into(),
+                    _ => "tea".into(),
+                }
+            }
+            
             // Tea time 16h and 22h
             if (utc_plus_2.hour() == 16 && utc_plus_2.minute() < 1)
                 || (utc_plus_2.hour() == 22 && utc_plus_2.minute() < 1)
             {
                 if let Err(why) = ChannelId(ZIGGURAT)
                     .send_message(&ctx, |m| {
-                        m.content("It's iced tea time!");
+                        m.content(format!("It's {} time!", beverage));
                         m.allowed_mentions(|am| am.parse(serenity::builder::ParseValue::Users));
                         m
                     })
