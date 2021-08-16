47ddf7a24b9fd2c9b6b6fb327765b7493c8c7042
Oscuro87 <capelle.e@gmail.com>
2021-08-16 23:16:17 +0200

    New shortcut to send embeds (experimental)

--- src/utils/shortcuts.rs
+++ src/utils/shortcuts.rs
@@ -1,7 +1,10 @@
 #![allow(dead_code)]
 
+use serenity::model::channel::Embed;
 use serenity::{model::id::ChannelId, prelude::*, utils::MessageBuilder};
 use serenity::http::CacheHttp;
+
+use crate::datastructs::CEmbedData;
 
 pub async fn send(ctx: &Context, target_channel: ChannelId, reply: &mut MessageBuilder) -> () {
     send_or_forward_err(ctx, target_channel, reply).await;
@@ -16,7 +19,9 @@ pub async fn send_or_console_err(ctx: &Context, target_channel: ChannelId, reply
 pub async fn send_or_discord_err(ctx: &Context, target_channel: ChannelId, error_target_channel: ChannelId, reply: &mut MessageBuilder) -> () {
     if let Some(err) = send_or_forward_err(ctx, target_channel, reply).await {
         let error_message: String = format!("Erreur: {}", err);
-        let _ = error_target_channel.say(ctx.http(), error_message).await;
+        if let Err(err2) = error_target_channel.say(ctx.http(), error_message).await {
+            println!("{}", err2);
+        }
     }
 }
 
@@ -30,4 +35,26 @@ pub async fn send_or_forward_err(
     }
 
     None
+}
+
+pub async fn send_embed_ignore_error(
+    ctx: &Context,
+    target_channel: ChannelId,
+    data: CEmbedData,
+) -> () {
+    let _ = target_channel.send_message(&ctx.http(), |m| {
+        m.content(data.content.clone());
+        m.tts(data.tts);
+
+        m.embed(|me| {
+            me.title(data.title.clone());
+            me.description(data.description.clone());
+            if data.thumbnail.is_some() {
+                me.thumbnail(data.thumbnail.unwrap());
+            }
+            me
+        });
+
+        m
+    });
 }
