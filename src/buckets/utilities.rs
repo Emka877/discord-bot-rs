e2d812df216c26c42a4e9dbdb12234e43952f4ed
Oscuro87 <capelle.e@gmail.com>
2021-11-06 18:11:31 +0100

    Switch user permission upon using !notabot command (if not already confirmed)

--- src/buckets/utilities.rs
+++ src/buckets/utilities.rs
@@ -1,13 +1,19 @@
-use serenity::{client::Context, framework::standard::{CommandResult, macros::command}, model::channel::Message, utils::MessageBuilder};
-use std::fs;
-use std::env::current_exe;
 use chrono::offset::Utc;
 use chrono::DateTime;
-use serenity::model::id::{MessageId, ChannelId};
+use serenity::model::id::{ChannelId, MessageId, RoleId};
+use serenity::{
+    client::Context,
+    framework::standard::{macros::command, CommandResult},
+    model::channel::Message,
+    utils::MessageBuilder,
+};
+use std::env::current_exe;
+use std::fs;
+use serenity::framework::standard::CommandError;
 
-use crate::{constants::channels::ERRORS, datastructs::SanitizedMessage};
 use crate::utils::shortcuts::send_or_discord_err;
-
+use crate::{constants::channels::ERRORS, datastructs::SanitizedMessage};
+use crate::constants::channels::INFRARED;
 
 #[command]
 pub async fn version(ctx: &Context, msg: &Message) -> CommandResult {
@@ -17,13 +23,15 @@ pub async fn version(ctx: &Context, msg: &Message) -> CommandResult {
     let build_tz = build_date + chrono::Duration::hours(2);
 
     msg.reply(
-        ctx, 
-        format!("\nAnna version {}\nBuilt on {}", 
+        ctx,
+        format!(
+            "\nAnna version {}\nBuilt on {}",
             env!("CARGO_PKG_VERSION"),
             build_tz
-        )
-    ).await?;
-    
+        ),
+    )
+    .await?;
+
     Ok(())
 }
 
@@ -38,7 +46,11 @@ pub async fn move_message_manually(ctx: &Context, msg: &Message) -> CommandResul
     let src_channel_id: ChannelId = msg.channel_id;
     let msg_id_parsed: u64 = san.arguments.get(0).unwrap().parse::<u64>().unwrap();
     let message_id: MessageId = MessageId(msg_id_parsed);
-    let message = ctx.http.get_message(src_channel_id.into(), message_id.into()).await.unwrap();
+    let message = ctx
+        .http
+        .get_message(src_channel_id.into(), message_id.into())
+        .await
+        .unwrap();
     let chan_id_parsed: u64 = san.arguments.get(1).unwrap().parse::<u64>().unwrap();
     let target_channel_id: ChannelId = ChannelId(chan_id_parsed);
     let original_poster_name: String = message.author.name.clone();
@@ -59,12 +71,30 @@ pub async fn move_message_manually(ctx: &Context, msg: &Message) -> CommandResul
 
     if del_result.is_ok() {
         // Send to new channel
-        let _ = send_or_discord_err(
-            ctx, 
-            target_channel_id, 
-            ERRORS.into(), 
-            &mut msg_builder
-        ).await;
+        let _ = send_or_discord_err(ctx, target_channel_id, ERRORS.into(), &mut msg_builder).await;
+    }
+
+    Ok(())
+}
+
+#[command]
+#[aliases("notabot")]
+pub async fn not_a_bot(ctx: &Context, msg: &Message) -> CommandResult {
+    let infrared_role_id = RoleId::from(INFRARED);
+    // let everyone_role_id = RoleId::from(EVERYONE);
+    let guild_id = msg.guild_id.unwrap();
+    let user = msg.author.clone();
+
+    if let Ok(is_infrared) = user.has_role(&ctx.http, guild_id, infrared_role_id).await {
+        if is_infrared {
+            let _ = msg.reply_mention(&ctx.http, "You're already a confirmed member, congratulations.").await;
+        } else {
+            if let Err(role_error) = &ctx.http.add_member_role(guild_id.into(), user.id.into(), infrared_role_id.into()).await {
+                eprintln!("Cannot assign role to user: {}", role_error.to_string());
+                return Err(CommandError::from("Cannot assign role to user: {}", role_error.to_string()));
+            }
+            let _ = msg.reply_mention(&ctx.http, "You are now confirmed.").await;
+        }
     }
 
     Ok(())
