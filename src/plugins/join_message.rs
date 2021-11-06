d9dc32466b7ca23a17339305341f6f96c4e3c4dd
Oscuro87 <capelle.e@gmail.com>
2021-11-06 18:11:11 +0100

    Use private messaging rather than channel

--- src/plugins/join_message.rs
+++ src/plugins/join_message.rs
@@ -1,15 +1,12 @@
-use crate::constants::channels::LANDING_CHANNEL;
-use serenity::model::id::ChannelId;
 use serenity::utils::MessageBuilder;
 use serenity::{client::Context, model::guild::Member};
 use std::sync::Arc;
-use crate::utils::shortcuts::send_or_console_err;
+use crate::utils::shortcuts::{send_private_message_or_console_error};
 
 pub async fn send_join_message(ctx: Arc<Context>, member: Member) {
     let mut builder = MessageBuilder::new();
     builder.push_line("Welcome, ");
     builder.user(member.clone());
     builder.push_line("! Please write or private message me `!notabot` to confirm joining this server.");
-    let landing_channel: ChannelId = ChannelId::from(LANDING_CHANNEL);
-    send_or_console_err(&ctx, landing_channel, &mut builder);
+    send_private_message_or_console_error(&ctx, member.user.id, &mut builder).await;
 }
